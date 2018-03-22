03b47eb1ace9e6f10f68de5235ea9b861ef8ce6a
Gustav Sivander <gustav.sivander@claremont.se>
2018-03-22 13:35:27 +0100

    update anyorigin to https

--- scripts/commute.js
+++ scripts/commute.js
@@ -1,7 +1,7 @@
 function updateCommute(){
     var url = "https://api.sl.se/api2/TravelplannerV3/trip.json?key=8b242adb1c364ae38ab3c91883be0348&originId=9192&destId=1002";
 
-    $.getJSON('http://anyorigin.com/go?url=https%3A//api.sl.se/api2/TravelplannerV3/trip.json%3Fkey%3D8b242adb1c364ae38ab3c91883be0348%26originId%3D9192%26destId%3D1002&callback=?', function(data){
+    $.getJSON('https://anyorigin.com/go?url=https%3A//api.sl.se/api2/TravelplannerV3/trip.json%3Fkey%3D8b242adb1c364ae38ab3c91883be0348%26originId%3D9192%26destId%3D1002&callback=?', function(data){
         setCommuteData(data.contents);
     });
 
