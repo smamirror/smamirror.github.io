70aab97750c7f495dd24ed824001440d560776d6
Gustav Sivander <gustav.sivander@claremont.se>
2018-03-22 13:13:41 +0100

    add cross domain test

--- scripts/commute.js
+++ scripts/commute.js
@@ -1,9 +1,16 @@
 function updateCommute(){
     var url = "http://api.sl.se/api2/TravelplannerV3/trip.json?key=8b242adb1c364ae38ab3c91883be0348&originId=9192&destId=1002";
 
-    $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', function(data){
+    /* $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', function(data){
         setCommuteData(data.contents);
-    });
+    }); */
+
+    $.ajax({
+        url: url,
+        dataType: "json"
+      }).done(function(data) {
+          console.log(data)
+      });
 }
 
 function setCommuteData(data){
